204020d4d6baa7e31acd24e09d106e18fbad4e77
xi <52056870+xigua222@users.noreply.github.com>
2023-11-07 20:06:51 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "xigua_go",
+    name: "xgda",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "say hello to the world.",
