f64234d51605d405e2fb1373f485441e925a0c8a
xi <52056870+xigua222@users.noreply.github.com>
2023-11-07 19:58:19 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,14 +1,10 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "xigua_go",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    bio: "say hello to the world.",
   },
   projects: [
     {
@@ -18,8 +14,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "xigua-go",
+    description: "welcome to xigua_go!",
   },
 
   // CONFIG configration (required)
